9c3decb5d7eff86ea3f06daead56387601e777a7
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2022-12-06 14:48:18 +0530

    Changes

--- mercator-to-lat-long/converter.py
+++ mercator-to-lat-long/converter.py
@@ -2,34 +2,48 @@
 """
 Created on Tue Dec  6 13:48:29 2022
 
-@author: Rishikesh
+@author: Rishikesh Sreehari
 """
 
 from pyproj import Proj, transform
 import pandas as pd
+import numpy as np
+import h5py
 
 
-cordinates = pd.read_csv('Y.csv')
-cordinates['X2'] = ''
-cordinates['Y2'] = ''
+# read coordinates in a dataframe
+df_y = pd.DataFrame(np.array(h5py.File("mer.h5")['Y']))
+df_x = pd.DataFrame(np.array(h5py.File("mer.h5")['X']))
+
+# combine dataframes
+df_comb = pd.concat([df_x,df_y], ignore_index=True, sort=False, axis=1)
+# name columns
+df_comb.set_axis(['X1','Y1'], axis=1, inplace=True)
+# convert coordinates to degree
+
+
+
+#cordinates = pd.read_csv('Y.csv')
+df_comb['X2'] = ''
+df_comb['Y2'] = ''
 
 
 def convert(x1,y1,i):
     inProj = Proj(init='epsg:3857')
     outProj = Proj(init='epsg:4326')
     x2,y2 = transform(inProj,outProj,x1,y1)
-    cordinates.loc[i,'X2'] = x2
-    cordinates.loc[i,'Y2'] = y2
+    df_comb.loc[i,'X2'] = x2
+    df_comb.loc[i,'Y2'] = y2
     return(x2,y2)
 
 
-for i in range(len(cordinates)):
-    x1=cordinates.loc[i,'X'] 
-    y1=cordinates.loc[i,'Y'] 
+for i in range(len(df_comb))[:2]:
+    x1=df_comb.loc[i,'X1'] 
+    y1=df_comb.loc[i,'Y1'] 
     convert(x1, y1, i)
     
 
-cordinates.to_csv("converted.csv", index=False)
+#cordinates.to_csv("converted.csv", index=False)
 
 
 
