3b1d2de81ea8fe4b152539b5b9d10c15b4722a20
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2023-01-08 23:24:44 +0530

    Updated code, readme

--- data-from-hdf5-file/final_converter.py
+++ data-from-hdf5-file/final_converter.py
@@ -8,11 +8,11 @@ Created on Fri Dec 16 11:28:16 2022
 import pandas as pd
 import h5py
 import time
-from pyproj import Proj, transform
+from pyproj import Proj, transform, transformer
+from pyproj import CRS
+from pyproj import Transformer
+import numpy as np
 
-
-input_epsg=24378
-output_epsg=4326
 
 start_time = time.time()
 
@@ -21,14 +21,22 @@ with h5py.File("mer.h5", "r") as file:
     df_X = pd.DataFrame(file.get("X")[:-2], columns=["X"])
     df_Y = pd.DataFrame(file.get("Y"), columns=["Y"])
     DHI = file.get("DHI")[0][:, :-2].reshape(-1)
+    projection= file['Projection_Information']
+    lat_0= float(projection.attrs['standard_parallel'])
+    lon_0= float(projection.attrs['longitude_of_projection_origin'])
+    
 
 final = df_Y.merge(df_X, how="cross").assign(DHI=DHI)[["X", "Y", "DHI"]]
 
 
+projString='+proj=merc +lat_ts='+str(lat_0)+' +lon_0='+str(lon_0)
+inProj=Proj(projString)
+outProj=Proj(init='epsg:4326')
 
-final['X2'],final['Y2']=transform(input_epsg,output_epsg,final[["X"]].to_numpy(),final[["Y"]].to_numpy(),always_xy=True)
 
+final['X2'],final['Y2']=transform(inProj,outProj,final[["X"]].to_numpy(),final[["Y"]].to_numpy())
+final.to_csv("plotted.csv", index=False)
 
-#final.to_csv("final_converted1.csv", index=False)
 
 print("--- %s seconds ---" % (time.time() - start_time))
+
